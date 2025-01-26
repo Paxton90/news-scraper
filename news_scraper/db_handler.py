47995b33bc6a14d856ac9fef153b7cc318d912ab
Paxton <paxton0505tw@gmail.com>
2025-01-26 14:29:24 +0800

    feat: save news articles to database

--- news_scraper/db_handler.py
+++ news_scraper/db_handler.py
@@ -23,5 +23,29 @@ def initialize_db():
     conn.commit()
     conn.close()
 
+def save_to_db(news_list):
+    """Save the news data into the SQLite database and count duplicates."""
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+
+    skipped_duplicates = 0
+    saved_news = []
+
+    for news in news_list:
+        try:
+            cursor.execute('''
+                INSERT INTO news (title, content, url, published_at)
+                VALUES (?, ?, ?, ?)
+            ''', (news["title"], news["content"], news["url"], news["published_at"]))
+            saved_news.append(news)  # Save the successfully inserted news
+        except sqlite3.IntegrityError:
+            # Skip duplicates since url is unique
+            skipped_duplicates += 1
+
+    conn.commit()
+    conn.close()
+
+    return skipped_duplicates, saved_news
+
 if __name__ == "__main__":
     initialize_db()
